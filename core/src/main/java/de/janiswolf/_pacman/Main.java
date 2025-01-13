cc54ce02394f9a83fd32dc7cb2311fe93c1a9785
xeboil <jeremydreier05@gmail.com>
2025-01-13 15:26:21 +0100

    jumpscare off/on

--- core/src/main/java/de/janiswolf/_pacman/Main.java
+++ core/src/main/java/de/janiswolf/_pacman/Main.java
@@ -2,6 +2,7 @@ package de.janiswolf._pacman;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
@@ -16,7 +17,7 @@ import de.janiswolf._pacman.entities.*;
 import java.util.ArrayList;
 
 public class Main extends ApplicationAdapter {
-    Texture backgroundTexture, entityTexture, entity2Texture, playerTexture;
+    Texture backgroundTexture, entityTexture, entity2Texture, playerTexture, deathScreenTexture;
     SpriteBatch spriteBatch;
     FitViewport viewport;
     int worldSize = 100;
@@ -24,33 +25,29 @@ public class Main extends ApplicationAdapter {
     private Player player;
     private EntitySpawner entitySpawner;
     OrthographicCamera camera;
+    private boolean gameOver;
+    private Sound deathSound;
 
     @Override
     public void create() {
-
         backgroundTexture = new Texture("background.png");
-
+        deathScreenTexture = new Texture("deathscreen.png"); // Assuming a death screen image
+        deathSound = Gdx.audio.newSound(Gdx.files.internal("deathsound.mp3")); // Load the death sound
 
         spriteBatch = new SpriteBatch();
         gridWorld = new GridWorld(worldSize);
         gridWorld.createGrid(worldSize, worldSize);
-        entitySpawner = new EntitySpawner(gridWorld,this,1,0.2f);
+        entitySpawner = new EntitySpawner(gridWorld, this, 1, 0.2f);
         entitySpawner.create(EntityType.PLAYER);
         entitySpawner.create(EntityType.DOG);
         entitySpawner.create(EntityType.SPEED_DOG);
         entitySpawner.create(EntityType.NO_COLLISION_DOG);
         entitySpawner.create(EntityType.DOG_INTERCEPTOR);
 
-        camera = new OrthographicCamera(player.getPosition().x,player.getPosition().y);
-        viewport = new FitViewport(25, 25,camera);
+        camera = new OrthographicCamera(player.getPosition().x, player.getPosition().y);
+        viewport = new FitViewport(25, 25, camera);
 
-
-
-
-
-
-
-
+        gameOver = false;  // Initially, the game is not over
     }
 
     @Override
@@ -62,10 +59,9 @@ public class Main extends ApplicationAdapter {
 
     @Override
     public void dispose() {
-
         backgroundTexture.dispose();
-
-
+        deathScreenTexture.dispose();
+        deathSound.dispose();
         playerTexture.dispose();
         spriteBatch.dispose();
     }
@@ -76,58 +72,53 @@ public class Main extends ApplicationAdapter {
     }
 
     private void input() {
-        for(Entity entity : entitySpawner.getEntities()){
-            if(entity instanceof Player){
-                Player player = (Player)entity;
-                player.update(Gdx.graphics.getDeltaTime());
+        if (!gameOver) {
+            for (Entity entity : entitySpawner.getEntities()) {
+                if (entity instanceof Player) {
+                    Player player = (Player) entity;
+                    player.update(Gdx.graphics.getDeltaTime());
+                }
             }
         }
-
     }
 
     private void logic() {
-
-
-
-        // Bounding-Rectangles für Spieler und Entitäten abrufen
-        Player player = null;
-        Rectangle playerBounds = null;
-        ArrayList<Rectangle> entityBounds = new ArrayList<>();
-        for(Entity entity : entitySpawner.getEntities() ){
-            if(!(entity instanceof Player)){
-                entityBounds.add(entity.getSprite().getBoundingRectangle());
-               Dog dog = (Dog) entity;
-               dog.update();
-            }else {
-                playerBounds = entity.getSprite().getBoundingRectangle();
-                player = (Player) entity;
+        if (!gameOver) {
+            // Bounding-Rectangles für Spieler und Entitäten abrufen
+            Player player = null;
+            Rectangle playerBounds = null;
+            ArrayList<Rectangle> entityBounds = new ArrayList<>();
+            for (Entity entity : entitySpawner.getEntities()) {
+                if (!(entity instanceof Player)) {
+                    entityBounds.add(entity.getSprite().getBoundingRectangle());
+                    Dog dog = (Dog) entity;
+                    dog.update();
+                } else {
+                    playerBounds = entity.getSprite().getBoundingRectangle();
+                    player = (Player) entity;
+                }
             }
-        }
 
+            // Überprüfe Kollision zwischen Spieler und Entität
+            for (Rectangle rectangle : entityBounds) {
+                if (playerBounds.overlaps(rectangle.set(rectangle.x, rectangle.y, 0.5f, 0.5f))) {
+                    if (!player.istTot()) {
+                        player.lebenVerloren();
+                        System.out.println("Leben verloren! Ein Leben verbleibend!");
 
-        // Überprüfe Kollision zwischen Spieler und Entität
-        for(Rectangle rectangle : entityBounds){
-
-            if (playerBounds.overlaps(rectangle.set(rectangle.x, rectangle.y, 0.5f, 0.5f))) {
-                if(!player.istTot()) {
-                    player.lebenVerloren();
-                    System.out.println("Leben verloren! Ein Leben verbleibend!");
-
-                    for (Entity entity : entitySpawner.getEntities()){
-                        entity.setPosition(new Vector2(entity.getStartX(),entity.getStartY()));
-                    }
-                    if(player.istTot()){
-                        System.out.println("Verloren! Kein Leben mehr übrig. LOSER!");
-                        Gdx.app.exit();
+                        for (Entity entity : entitySpawner.getEntities()) {
+                            entity.setPosition(new Vector2(entity.getStartX(), entity.getStartY()));
+                        }
+                        if (player.istTot()) {
+                            System.out.println("Verloren! Kein Leben mehr übrig. LOSER!");
+                            Gdx.app.exit();
+                            //deathSound.play(); // Play death sound
+                            //gameOver = true;  // Set game over state to true
+                        }
                     }
                 }
             }
         }
-
-
-
-
-
     }
 
     private void draw() {
@@ -135,16 +126,19 @@ public class Main extends ApplicationAdapter {
         viewport.apply();
         spriteBatch.setProjectionMatrix(viewport.getCamera().combined);
         spriteBatch.begin();
-        camera.position.set(player.getPosition().x,player.getPosition().y,0);
-        camera.update();
 
-
-        spriteBatch.draw(backgroundTexture, 0, 0, worldSize, worldSize);
-        for (Sprite wallSprite : gridWorld.getWallSprites()) {
-            wallSprite.draw(spriteBatch);
-        }
-        for (Entity entity : entitySpawner.getEntities()){
-            entity.getSprite().draw(spriteBatch);
+        if (gameOver) {
+            spriteBatch.draw(deathScreenTexture, 0, 0, 20, 20);  // Draw the death screen
+        } else {
+            camera.position.set(player.getPosition().x, player.getPosition().y, 0);
+            camera.update();
+            spriteBatch.draw(backgroundTexture, 0, 0, worldSize, worldSize);
+            for (Sprite wallSprite : gridWorld.getWallSprites()) {
+                wallSprite.draw(spriteBatch);
+            }
+            for (Entity entity : entitySpawner.getEntities()) {
+                entity.getSprite().draw(spriteBatch);
+            }
         }
 
         spriteBatch.setProjectionMatrix(camera.combined);
